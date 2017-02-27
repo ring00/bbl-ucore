04f3e42f6356c183200794e38ebfc28e20410c69
zwei <suvtab@gmail.com>
2017-02-27 12:02:54 +0800

    fix a bug in do_pagefault

--- labcodes/lab3/kern/mm/vmm.c
+++ labcodes/lab3/kern/mm/vmm.c
@@ -366,7 +366,7 @@ do_pgfault(struct mm_struct *mm, uint32_t error_code, uintptr_t addr) {
     */
 
     /*LAB3 EXERCISE 1: YOUR CODE*/
-    ptep = get_pte(mm->pgdir, addr, true);  //(1) try to find a pte, if pte's
+    ptep = get_pte(mm->pgdir, addr, 1);  //(1) try to find a pte, if pte's
                                             //PT(Page Table) isn't existed, then
                                             //create a PT.
     if (*ptep == 0) {
@@ -374,28 +374,38 @@ do_pgfault(struct mm_struct *mm, uint32_t error_code, uintptr_t addr) {
                                                   //exist, then alloc a page &
                                                   //map the phy addr with
                                                   //logical addr
-    }
-    else {
-    /*LAB3 EXERCISE 2: YOUR CODE
-    * Now we think this pte is a  swap entry, we should load data from disk to a page with phy addr,
-    * and map the phy addr with logical addr, trigger swap manager to record the access situation of this page.
-    *
-    *  Some Useful MACROs and DEFINEs, you can use them in below implementation.
-    *  MACROs or Functions:
-    *    swap_in(mm, addr, &page) : alloc a memory page, then according to the swap entry in PTE for addr,
-    *                               find the addr of disk page, read the content of disk page into this memroy page
-    *    page_insert ： build the map of phy addr of an Page with the linear addr la
-    *    swap_map_swappable ： set the page swappable
-    */
-        if(swap_init_ok) {
-            struct Page *page=NULL;
-            swap_in(mm, addrm &page);                        //(1）According to the mm AND addr, try to load the content of right disk page
-                                    //    into the memory which page managed.
-            page_insert(pgdir, page, addr, perm);                        //(2) According to the mm, addr AND page, setup the map of phy addr <---> logical addr
-            swap_map_swappable(mm, addr, page, )                        //(3) make the page swappable.
-        }
-        else {
-            cprintf("no swap_init_ok but ptep is %x, failed\n",*ptep);
+    } else {
+        /*LAB3 EXERCISE 2: YOUR CODE
+        * Now we think this pte is a  swap entry, we should load data from disk
+        * to a page with phy addr,
+        * and map the phy addr with logical addr, trigger swap manager to record
+        * the access situation of this page.
+        *
+        *  Some Useful MACROs and DEFINEs, you can use them in below
+        * implementation.
+        *  MACROs or Functions:
+        *    swap_in(mm, addr, &page) : alloc a memory page, then according to
+        * the swap entry in PTE for addr,
+        *                               find the addr of disk page, read the
+        * content of disk page into this memroy page
+        *    page_insert ： build the map of phy addr of an Page with the linear
+        * addr la
+        *    swap_map_swappable ： set the page swappable
+        */
+        if (swap_init_ok) {
+            struct Page *page = NULL;
+            swap_in(mm, addr, &page);  //(1）According to the mm AND addr, try
+                                       //to load the content of right disk page
+                                       //    into the memory which page managed.
+            page_insert(pgdir, page, addr, perm);  //(2) According to the mm,
+                                                   //addr AND page, setup the
+                                                   //map of phy addr <--->
+                                                   //logical addr
+            swap_map_swappable(mm, addr, page,
+                               0);  //(3) make the page swappable.
+            page->pra_vaddr = addr;
+        } else {
+            cprintf("no swap_init_ok but ptep is %x, failed\n", *ptep);
             goto failed;
         }
    }
