363a97e05556fbdb3aa703352654e140a17b1986
zwei <suvtab@gmail.com>
2017-03-02 09:30:11 +0800

    fix the bug

--- labcodes/lab6/kern/mm/pmm.c
+++ labcodes/lab6/kern/mm/pmm.c
@@ -528,6 +528,10 @@ copy_range(pde_t *to, pde_t *from, uintptr_t start, uintptr_t end, bool share) {
          * (3) memory copy from src_kvaddr to dst_kvaddr, size is PGSIZE
          * (4) build the map of phy addr of  nage with the linear addr start
          */
+        uintptr_t src_kvaddr = page2kva(page);
+        uintptr_t dst_kvaddr = page2kva(npage);
+        memcpy(dst_kvaddr, src_kvaddr, PGSIZE);
+        page_insert(to, npage, start, perm);
         assert(ret == 0);
         }
         start += PGSIZE;
