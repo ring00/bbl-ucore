a91ded414195812aad6e22d7b4bf9f507ee67db4
Zwei <suvtab@gmail.com>
2017-03-02 09:42:24 +0800

    set BIG_STRIDE to correct value

--- labcodes/lab6/kern/schedule/default_sched_stride.c
+++ labcodes/lab6/kern/schedule/default_sched_stride.c
@@ -8,7 +8,7 @@
 
 /* You should define the BigStride constant here*/
 /* LAB6: YOUR CODE */
-#define BIG_STRIDE 1 << 20 /* you should give a value, and is ??? */
+#define BIG_STRIDE 0x7FFFFFFF /* you should give a value, and is ??? */
 
 /* The compare function for two skew_heap_node_t's and the
  * corresponding procs*/
